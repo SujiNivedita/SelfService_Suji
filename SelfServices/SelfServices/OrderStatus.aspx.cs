cddd23f1ce40eadee95f934f95b5ab3f0d742165
SujiNivedita <sujishines@gmail.com>
2015-08-26 23:50:31 +0530

    trail2

---
+++ SelfServices/SelfServices/OrderStatus.aspx.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SelfServices.Models;
+using SelfServices.Utilities;
+
+namespace SelfServices
+{
+    public partial class OrderStatus : System.Web.UI.Page
+    {
+        Profile p;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session["customerId"] = "VZ/1234";
+            if (Session["customerId"] != null)
+            {
+                string cid = (string)Session["customerId"];
+                p = Profile.GetUserProfile(cid);
+                List<string> orderId = new List<string>();
+                if (p != null)
+                {
+                    foreach (OrderWrapper orders in p.Orders)
+                    {
+                        if (orders.Status != "cancelled")
+                        {
+                            orderId.Add(orders.Id);
+                        }
+
+                    }
+                    DropDownList1.DataSource = orderId;
+                    DropDownList1.DataBind();
+                    string order_id = p.Orders[0].Id;
+                    DateTime order_date = p.Orders[0].OrderDate;
+                    DateTime instal_date = Extensions.ToDateTime(p.Orders[0].InstallationDate.ToString());
+                    lbl_instal.Text = "Your order is scheduled installation on:";
+                    btnDay.Visible = true;
+                    btnDay.Text = instal_date.Day.ToString();
+                    btnmonth.Visible = true;
+                    btnmonth.Text = instal_date.Month.ToString();
+                    btnyear.Visible = true;
+                    btnyear.Text = instal_date.Year.ToString();
+                    btnChangeDate.Visible = true;
+                    btnCancelOrder.Visible = true;
+                    lbl_esti.Text = "Estimated Time :";
+                    lbltime.Text = "3hrs 45 mins";
+                    lbl_serv.Text = "Service Address";
+                    lblServ.Text=p.ServiceAddress;
+                    lbl_ordrinfo.Text = "Order Info";
+                    lbl_prodinfo.Text = "Included in this order ";
+                    Repeater1.DataSource = p.Orders[0].Services;
+                    Repeater1.DataBind();
+
+                }
+            }
+            else
+            {
+                Response.Redirect("index.aspx");
+            }
+
+
+        }
+
+        protected void btnCancelOrder_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string cid = (string)Session["customerId"];
+            p = Profile.GetUserProfile(cid);
+            string orderid = DropDownList1.SelectedItem.Value;
+            foreach (OrderWrapper oid in p.Orders)
+            {
+                if (oid.Id == orderid)
+                {
+                    string status = oid.Status;
+                    string order_date = oid.OrderDate;
+                   DateTime instal_date =p.Orders[0].InstallationDate;
+                    if (status == "new" || status == "out provision")
+                    {
+                        lbl_instal.Text = "Your order is scheduled installation on:";
+                        btnDay.Visible = true;
+                        btnDay.Text = instal_date.Day.ToString();
+                        btnmonth.Visible = true;
+                        btnmonth.Text = instal_date.Month.ToString();
+                        btnyear.Visible = true;
+                        btnyear.Text = instal_date.Year.ToString();
+                        btnChangeDate.Visible = true;
+                        btnCancelOrder.Visible = true;
+                        lbl_esti.Text = "Estimated Time :";
+                        lbltime.Text = "1hrs 15 mins";
+                        lbl_serv.Text = "Service Address";
+                        lblServ.Text=p.ServiceAddress;
+                        lbl_ordrinfo.Text = "Your Products Info";
+                        lbl_prodinfo.Text = "Included in this order ";
+                        Repeater1.DataSource = oid.Services;
+                        Repeater1.DataBind();
+                    }
+                    if (status == "in provision")
+                    {
+                        lbl_instal.Text = "Your order is scheduled installation on:";
+                        btnDay.Visible = true;
+                        btnDay.Text = instal_date.Day.ToString();
+                        btnmonth.Visible = true;
+                        btnmonth.Text = instal_date.Month.ToString();
+                        btnyear.Visible = true;
+                        btnyear.Text = instal_date.Year.ToString();
+                        btnChangeDate.Visible = true;
+                        btnCancelOrder.Visible = false;
+                        lbl_esti.Text = "Estimated Time :";
+                        lbltime.Text = "1hrs 15 mins";
+                        lbl_serv.Text = "Service Address";
+                        lblServ.Text = p.ServiceAddress;
+                        lbl_ordrinfo.Text = "Your Products Info";
+                        lbl_prodinfo.Text = "Included in this order ";
+                        Repeater1.DataSource = oid.Services;
+                        Repeater1.DataBind();
+                    }
+                    if (status == "Completed")
+                    {
+                        lbl_instal.Text = "Your order was Successfully completed!";
+                        lblInstal.Text = "Installed on";
+                        btnDay.Visible = true;
+                        btnDay.Text = instal_date.Day.ToString();
+                        btnmonth.Visible = true;
+                        btnmonth.Text = instal_date.Month.ToString();
+                        btnyear.Visible = true;
+                        btnyear.Text = instal_date.Year.ToString();
+                        btnChangeDate.Visible = false;
+                        btnCancelOrder.Visible = false;
+                        lbl_esti.Text = "Estimated Time :";
+                        lbltime.Text = "1hrs 15 mins";
+                        lbl_serv.Text = "Service Address";
+                        lblServ.Text = p.ServiceAddress;
+                        lbl_ordrinfo.Text = "Your Products Info";
+                        lbl_prodinfo.Text = "Included in this order ";
+                        Repeater1.DataSource = oid.Services;
+                        Repeater1.DataBind();
+                    }
+                }
+                //if (DropDownList1.SelectedItem.Value == "SampleId1")
+                //{
+
+                //    lbl_instal.Text = "Your order is scheduled installation on:";
+                //    btnDay.Visible = true;
+                //    btnmonth.Visible = true;
+                //    btnyear.Visible = true;
+                //    btnChangeDate.Visible = true;
+                //    btnCancelOrder.Visible = true;
+                //    btnCancelOrder.Enabled = false;
+                //    btnCancelOrder.ToolTip = "Your order has crossed the cancellation stage";
+                //    lbl_esti.Text = "Estimated Time :";
+                //    lbltime.Text = "3hrs 45 mins";
+                //    lbl_serv.Text = "service address";
+                //    lbl_ordrinfo.Text = "Order Info";
+                //    lbl_prodinfo.Text = "Included in this order ";
+                //}
+                //if (DropDownList1.SelectedItem.Value == "SampleId3")
+                //{
+                //    lbl_instal.Text = "Your order is complete!Installed on:";
+                //    btnDay.Visible = true;
+                //    btnmonth.Visible = true;
+                //    btnyear.Visible = true;
+                //    btnChangeDate.Visible = false;
+                //    btnCancelOrder.Visible = false;
+                //    lbl_esti.Text = "";
+                //    lbltime.Text = "";
+                //    lbl_serv.Text = "service address";
+                //    lbl_ordrinfo.Text = "Order Info";
+                //    lbl_prodinfo.Text = "Included in this order ";
+
+                //}
+                //if (DropDownList1.SelectedItem.Value == "sample_id2")
+                //{
+                //    lbl_instal.Text = "Your order is scheduled installation on:";
+                //    btnDay.Visible = true;
+                //    btnmonth.Visible = true;
+                //    btnyear.Visible = true;
+                //    btnChangeDate.Visible = true;
+                //    btnCancelOrder.Visible = true;
+                //    lbl_esti.Text = "Estimated Time :";
+                //    lbltime.Text = "3hrs 45 mins";
+                //    lbl_serv.Text = "service address";
+                //    lbl_ordrinfo.Text = "Order Info";
+                //    lbl_prodinfo.Text = "Included in this order ";
+
+                //}
+            }
+        }
+    }
+}
---
+++ SelfServices/SelfServices/faqControl.ascx
@@ -0,0 +1,126 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="faqControl.ascx.cs" Inherits="SelfServices.faqControl" %>
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="cc1" %>
+
+<%--<link rel="stylesheet" href="Content/bootstrap.css" />--%>
+<style type="text/css">
+    .btn-danger {
+        color: #ffffff;
+        background-color: #d9534f;
+        border-color: #d43f3a;
+    }  
+    .modalBackground
+    {
+        background-color: Black;
+        filter: alpha(opacity=60);
+        opacity: 0.6;
+    }
+    .modalPopup
+    {
+        background-color: #FFFFFF;
+        width: 300px;
+        border: 3px solid red;
+        border-radius: 12px;
+        padding:0
+      
+    }
+    .modalPopup .header
+    {
+        background-color: red;
+        height: 30px;
+        color: White;
+        line-height: 30px;
+        text-align: center;
+        font-weight: bold;
+        border-top-left-radius: 6px;
+        border-top-right-radius: 6px;
+    }
+    .modalPopup .body
+    {
+        min-height: 50px;
+        line-height: 30px;
+        text-align: center;
+        font-weight: bold;
+    }
+    .modalPopup .footer
+    {
+        padding: 6px;
+    }
+    .modalPopup .yes, .modalPopup .no
+    {
+        height: 23px;
+        color: Black;
+        line-height: 23px;
+        text-align: center;
+        font-weight: bold;
+        cursor: pointer;
+        border-radius: 4px;
+    }
+    .modalPopup .yes
+    {
+        background-color: #2FBDF1;
+        border: 1px solid #0DA9D0;
+    }
+    .modalPopup .no
+    {
+        background-color: #9F9F9F;
+        border: 1px solid #5C5C5C;
+    }
+</style>
+
+<p>
+    <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Size="X-Large" Font-Underline="True" ForeColor="#3399FF" Text="We're here to help you..." Font-Names="Cooper Black"></asp:Label>
+</p>
+<p>
+
+    <asp:LinkButton ID="LinkButton1"  PostBackUrl="//www.youtube.com/embed/caELQ4juL0Y?rel=0&amp;autoplay=1" runat="server" >Troubleshoot your  FiOS Router</asp:LinkButton>
+</p>
+<p>
+    <asp:LinkButton ID="LinkButton3"  PostBackUrl="//www.youtube.com/embed/NmqQsxqLqaU?rel=0&amp;autoplay=1" runat="server" >Improving your gaming connnection</asp:LinkButton>
+</p>
+<p>
+    <asp:LinkButton ID="LinkButton2" PostBackUrl="//www.youtube.com/embed/a9q-tDRCTtc?rel=0&amp;autoplay=1" runat="server">How to create a secure password</asp:LinkButton>
+</p>
+<p>
+    <asp:LinkButton ID="LinkButton4"  PostBackUrl="//www.youtube.com/embed/affSl7XxBmE?list=PLooRvxIU8b2WAMTuKzQ2nxKxoa1q6qWv5?rel=0&amp;autoplay=1" runat="server">How to Program your FiOS TV Remote</asp:LinkButton>
+</p>
+<div class="jumbotron" style="height: 519px; width: 289px; ">
+    <br />
+    &nbsp;<asp:Label ID="Label2" runat="server" Font-Bold="True" Font-Italic="True" Font-Names="Cooper Black" Font-Size="X-Large" ForeColor="#999966" Text="Tell us your Queries and Get Instant Answers"></asp:Label>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br />
+&nbsp;<asp:Image ID="Image1" runat="server" Height="158px" ImageUrl="~/images/calcentre.jpg" Width="234px" />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <br />
+    
+    <asp:Button ID="ask" class="btn btn-danger" runat="server" style="z-index: 1; left: 25px; top: 0px; position: relative; margin-bottom: 0px; width: 237px; height: 61px;" Text="Ask Us" OnClick="ask_Click" />
+    <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;
+    <br />
+    <cc1:ModalPopupExtender ID="mpe"  runat="server" PopupControlID="pnlPopup" TargetControlID="ask"
+    OkControlID="btnYes" CancelControlID="btnNo" BackgroundCssClass="modalBackground"></cc1:ModalPopupExtender>
+<asp:Panel ID="pnlPopup" runat="server" CssClass="modalPopup" Style="display: none">
+    <div class="header">
+        Enter your Query:
+    </div>
+    <div class="body">
+        Mail Adress:<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        Query:<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+    </div>
+    <div class="footer" align="right">
+        <asp:Button ID="btnYes" runat="server" Text="Submit" CssClass="yes" />
+        <asp:Button ID="btnNo" runat="server" Text="Cancel" CssClass="no" />
+    </div>
+</asp:Panel>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+</div>
+
+
+<p>
+    &nbsp;</p>
+<p>
+    &nbsp;</p>
+
+
+
